9babb48c7eb652cf9445b461a409bac46656f2a7
lucas.lima <lucas.lima@TI-001.FARMARCAS.CORP>
2018-10-04 08:39:27 -0300

    Feito o exercicio 03 e 05 do curso logica capitulo 02

--- CursoLogicaCapitulo03/src/Exercicio05.java
+++ CursoLogicaCapitulo03/src/Exercicio05.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
 
 public class Exercicio05 {
+
+	static final Integer NOTA_MINIMA_PARA_PASSAR_DE_ANO = 7;
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.printf("Informe a nota do aluno: ");
+		Double notaDoAluno = scanner.nextDouble();
+
+		Boolean passouDeAno = notaDoAluno >= NOTA_MINIMA_PARA_PASSAR_DE_ANO;
+
+		if (passouDeAno) {
+			System.out.println("Parabéns!, Aprovado.");
+		} else {
+			System.out.println("Reprovado! Precisa estudar mais.");
+		}
+
+		scanner.close();
+
+	}
 
 }
