836b49768f427b85c34382fc1570e1f437140846
lucas.lima <lucas.lima@TI-001.FARMARCAS.CORP>
2018-10-04 17:24:57 -0300

    Commit da classe Concatenacao Textos

---
+++ CursoLogicaCapitulo04/src/ConcatenacaoTexto.java
@@ -0,0 +1,17 @@
+
+public class ConcatenacaoTexto {
+	
+	public static void main(String[] args) {
+		
+		String inicio = "Esse é o ";
+		String meio = "º exemplo ";
+		String fim = "de concatenação de textos.";
+		Integer numeroExemplo = 1;
+		
+		System.out.println(inicio + numeroExemplo + meio + fim);
+		
+		//System.out.println("Esse " + meio + "de concatenação de textos.");
+		
+	}
+
+}
