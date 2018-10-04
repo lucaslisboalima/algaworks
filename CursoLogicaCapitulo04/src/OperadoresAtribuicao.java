52172a2ea384c3678889995d5cfd0ed7604e0a9d
lucas.lima <lucas.lima@TI-001.FARMARCAS.CORP>
2018-10-04 12:07:05 -0300

    Commit da classe Operadores de atribuição

---
+++ CursoLogicaCapitulo04/src/OperadoresAtribuicao.java
@@ -0,0 +1,29 @@
+
+public class OperadoresAtribuicao {
+	
+	public static void main(String[] args) {
+		
+		//Integer numero = 7;
+		Double numero = 7.0;
+		
+//		numero = numero + 4;
+//		System.out.println("Número + 4: " + numero);
+		
+//		numero += 4;
+//		System.out.println("Número += 4: " + numero);
+		
+//		numero -= 4;
+//		System.out.println("Número -=: " + numero);
+		
+//		numero *= 4;
+//		System.out.println("Número *=: " + numero);
+		
+//		numero /= 4;
+//		System.out.println("Número /=: " + numero);
+		
+		numero %= 4;
+		System.out.println("Número %=: " + numero);
+		
+	}
+
+}
