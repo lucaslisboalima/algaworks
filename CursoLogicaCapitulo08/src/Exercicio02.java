8afd9c7a48eb0af2a5153de3fed381cd82045dd6
lucas.lima <lucas.lima@TI-001.FARMARCAS.CORP>
2018-10-30 15:57:52 -0300

    Feito exercicio 02 do curso de logica capitulo 08

---
+++ CursoLogicaCapitulo08/src/Exercicio02.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+public class Exercicio02 {
+	
+	public static void main(String[] args) {
+		
+Scanner scanner = new Scanner(System.in);
+		
+		imprimirTraco();
+		
+		String[] cursos = new String[]{"Java EE", "Spring", "Java OO Avançado"};
+		
+		System.out.println("Escolha dentre os cursos abaixo");
+		
+		iterarEExibirPosicoesDoVetorString(cursos);
+		
+		imprimirEContinuarNaMesmaLinha("O curso que você deseja é o: ");
+		Integer posicaoCursoEscolhido = scanner.nextInt();
+		
+		Boolean validaPosicao = posicaoCursoEscolhido >= 0 && posicaoCursoEscolhido < cursos.length;
+		
+		if(!validaPosicao){
+			encerrarProgramaPorCausaDePosicaoInvalida();
+		}
+		
+		
+		
+		String[] formasPagamento = new String[]{"Boleto", "Cartão de Crédito"};
+		
+		System.out.println("Escolha dentre as formas de pagamento: ");
+		
+		iterarEExibirPosicoesDoVetorString(formasPagamento);
+		
+		imprimirEContinuarNaMesmaLinha("Sua forma de pagamento escolhida é: ");
+		Integer posicaoFormaPagamentoEscolhida = scanner.nextInt();
+		
+		validaPosicao = posicaoFormaPagamentoEscolhida >= 0 && posicaoFormaPagamentoEscolhida < formasPagamento.length;
+		
+		if(!validaPosicao){
+			encerrarProgramaPorCausaDePosicaoInvalida();
+		}
+		
+		String cursoEscolhido = cursos[posicaoCursoEscolhido];
+		String formaPagamentoEscolhido = formasPagamento[posicaoFormaPagamentoEscolhida];
+		
+		imprimirTraco();
+		
+		imprimir("O curso escolhido foi " + cursoEscolhido + " e forma de pagamento é " + formaPagamentoEscolhido + ".");
+		
+		scanner.close();
+	}
+	
+	static void imprimirTraco(){
+		System.out.println("------------------------------");
+	}
+	
+	static void encerrarProgramaPorCausaDePosicaoInvalida(){
+		System.err.println("Posição inválida!");
+		System.exit(1);
+	}
+	
+	static void imprimir(String texto){
+		System.out.println(texto);
+	}
+	
+	static void iterarEExibirPosicoesDoVetorString(String[] vetor){
+		for(int i = 0; i < vetor.length; i++){
+			System.out.println("[" + i + "]" + vetor[i]);
+		}
+	}
+	
+	static void imprimirEContinuarNaMesmaLinha(String texto){
+		System.out.println(texto);
+	}
+		
+}
